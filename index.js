edc720d4dc51ebf44882b9ec1194e765a058d179
DavidGT0 <gazit.david@gmail.com>
2025-12-26 08:23:57 +0200

    new updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const port = process.env.PORT;
 const api = process.env.HOST;
 const app = express();
-app.use(express.static(__dirname));
+
 app.use(express.static(path.join(__dirname , 'public')));
 app.use(express.json());
 app.use(cookies());
