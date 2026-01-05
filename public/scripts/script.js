8512827abe3fb41f0bf7545a2ffb044a85913969
DavidGT0 <gazit.david@gmail.com>
2026-01-05 20:17:47 +0200

    new updated new

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,2 +1,102 @@
-let greating = "בוקר טוב";
+let greating = "Hello ";
+greating += localStorage.getItem('name');
 document.getElementById('greating').innerHTML = greating;
+allCategories = [];
+
+async function getTasks() {
+    try {
+        let response = await fetch('/tasks');
+        if (response.status == 401) {
+            window.location.href = '/login';
+            return;
+        }
+        let data = await response.json();
+        if (response.status == 400) {
+            alert(data.message);
+            return;
+        }
+        createTable(data);
+    } catch (err) {
+        alert(err)
+    }
+}
+
+async function getCategories() {
+    try {
+        let response = await fetch('/categories');
+        if (response.status == 401) {
+            window.location.href = '/login';
+            return;
+        }
+        let data = await response.json();
+        if (response.status == 400) {
+            alert(data.message);
+            return;
+        }
+        for(let c of data){
+            allCategories[c.id] = c;
+        }
+        SelectCat();
+    } catch (err) {
+        alert(err)
+    }
+}
+
+function createTable(data) {
+    let txt = "";
+    for (obj of data) {
+        if (obj) {
+            let isChecked = obj.is_done ? "checked" : "";
+            let rowClass = obj.is_done ? "class='rowClass'" : "";
+            txt += `<tr ${rowClass}>`;
+            txt += `<td><input type="checkbox" ${isChecked} onchange="taskDone(${obj.id},this)"></td>`;
+            txt += `<td>${obj.text}</td>`;
+            txt += `<td>${obj.category_id}</td>`;
+            txt += `<td><button onclick="deleteTask(${obj.id})">🗑️</button></td>`;
+            txt += `<td><button onclick="taskToEdit(${obj.id})">✏️</button></td>`;
+            txt += "</tr>";
+        }
+    }
+    document.getElementById('myTable').innerHTML = txt;
+}
+
+async function taskDone(id, elm) {
+    let isDone = elm.checked;
+    try {
+        let response = await fetch(`/tasks/${id}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ isDone })
+        })
+        getTasks();
+    } catch (err) {
+        alert(err)
+    }
+}
+
+function SelectCat(){
+    let select = document.getElementById('tasksSelect');
+    select.innerHTML = '<option value="">View All Tasks</option>';
+    allCategoris.forEach(category =>{
+        const option = document.createElement('option');
+        option.value = category.id;
+        option.textContent = category.name;
+        select.appendChild(option);
+    })
+    select.addEventListener('change', filterTasks);
+}
+
+function filterTasks() {
+    let select = document.getElementById('tasksSelect');
+    let selectedCategoryId = select.value;
+
+    if (selectedCategoryId === '') {
+        createTable(allTasks);
+    } else {
+        let filteredTasks = allTasks.filter(task => task.category_id == selectedCategoryId);
+        createTable(filteredTasks);
+    }
+}
+
+getCategories();
+getTasks();
--- routes/tasks_R.js
+++ routes/tasks_R.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {getAllTasks,addTask,getTask,deleteTask,editTask} = require('../controller/tasks_C');
-const {isLoggedIn} = require('../middelware/tasks_MID');
+const {isLoggedIn} = require('../middelware/auth_MID');
 const {valuesToAdd,isValidId,valuesToEdit} = require('../middelware/tasks_MID');
 
 
 router.get('/',isLoggedIn,getAllTasks);
-routrouter.post('/',isLoggedIn,valuesToAdd,addTask);
+router.post('/',isLoggedIn,valuesToAdd,addTask);
 router.get('/:id',isLoggedIn,isValidId,getTask);
 router.delete('/:id',isLoggedIn,isValidId,deleteTask);
 router.patch('/:id',isLoggedIn,isValidId,valuesToEdit,editTask);
