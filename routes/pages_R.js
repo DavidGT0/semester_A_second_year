f983044444475c7f4694b1c3bcc89e221140d957
DavidGT0 <gazit.david@gmail.com>
2026-01-25 18:54:18 +0200

    הוספת לוגין ליוזרס ולקטגוריס

--- routes/pages_R.js
+++ routes/pages_R.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const {isLoggedIn} = require('../middelware/auth_MID');
 
 router.get('/',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","index.html"))})
 router.get('/reg',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","reg.html"))})
 router.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","login.html"))})
+router.get('/users',isLoggedIn,(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","users.html"))})
+router.get('/categories',isLoggedIn,(req,res)=>{res.sendFile(path.join(__dirname,"..","public","pages","categories.html"))})
 
 module.exports = router;
